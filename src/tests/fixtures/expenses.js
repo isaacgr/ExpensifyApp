6074301d4a831f8eebea0ea33ea9ff16f4688f8f
isaacgr <isaacgrowell@gmail.com>
2018-06-05 07:41:47 -0400

    Add setExpense action

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -3,19 +3,19 @@ import moment from 'moment';
 export default [{
   id: '1',
   description: 'Car',
-  note: '',
+  note: 'Its a car',
   amount: 100,
-  createdAt: moment(0)
+  createdAt: moment(0).valueOf()
 }, {
   id: '2',
   description: 'Rent',
-  note: '',
+  note: 'Its rent',
   'amount': 10000,
   createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'Credit Card',
-  note: '',
+  note: 'Its a debt',
   'amount': 1000,
   createdAt: moment(0).add(4, 'days').valueOf()
 }]
