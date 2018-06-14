3dbdf2e29c407ffed865847fac5d30d9cf01c2b6
isaacgr <isaacgrowell@gmail.com>
2018-06-14 07:44:05 -0400

    Add authentication

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import './firebase/firebase';
 import { firebase } from './firebase/firebase';
@@ -32,6 +32,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user){
+    store.dispatch(login(user.id))
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -39,6 +40,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     })
   } else {
+    store.dispatch(logout())
     renderApp();
     history.push('/')
   }
